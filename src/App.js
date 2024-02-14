48261bf574ad226bf5d22ec2c86f80b412e5926a
AchiGel <ar.gelashvili@gmail.com>
2024-02-14 15:34:19 +0400

    add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="shopping-card">
+        <h1>Shopping List</h1>
+        <div className="add-items-box">
+          <input type="text" placeholder="Add an item..." />
+          <button>+</button>
+        </div>
+        <div className="items-list">
+          <ul>
+            <li>item 1</li>
+            <li>item 1</li>
+            <li>item 1</li>
+          </ul>
+        </div>
+        <div className="total-amount">
+          <h2>Total: 1</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
