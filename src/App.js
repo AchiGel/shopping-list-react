b4c0905838b16dd1789514b61f87b37bf402e70c
AchiGel <ar.gelashvili@gmail.com>
2024-02-14 16:11:05 +0400

    add changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [listItem, setListItem] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleInputValue(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleAddList() {
+    if (inputValue.trim() !== "") {
+      setListItem([...listItem, inputValue]);
+      setInputValue("");
+    }
+  }
+
   return (
     <div className="App">
       <div className="shopping-card">
         <h1>Shopping List</h1>
         <div className="add-items-box">
-          <input type="text" placeholder="Add an item..." />
-          <button>+</button>
+          <input
+            type="text"
+            placeholder="Add an item..."
+            value={inputValue}
+            onChange={handleInputValue}
+          />
+          <button onClick={handleAddList}>+</button>
         </div>
         <div className="items-list">
           <ul>
-            <li>item 1</li>
-            <li>item 1</li>
-            <li>item 1</li>
+            {listItem.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
           </ul>
         </div>
         <div className="total-amount">
-          <h2>Total: 1</h2>
+          <h2>Total: {listItem.length}</h2>
         </div>
       </div>
     </div>
